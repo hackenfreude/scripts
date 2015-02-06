a61bae4ca67a69a7867240eba65eb73efb50be0a
hackenfreude <hackenfreude@users.noreply.github.com>
2015-02-05 22:24:27 -0800

    apt-get update/upgrade/install kept back/autoremove at top of script

--- machinesetup.sh
+++ machinesetup.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-echo 'remove unneeded packages'
-sudo apt-get remove deja-dup -y
-sudo apt-get remove gnome-bluetooth -y
-sudo apt-get remove gnome-mahjongg -y
-sudo apt-get remove gnome-mines -y
-sudo apt-get remove aisleriot -y
-sudo apt-get remove gnome-sudoku -y
-sudo apt-get remove libre-office-draw -y
-sudo apt-get remove webbrowser-app -y
-sudo apt-get remove thunderbird -y
-sudo apt-get remove libre-office-writer -y
-sudo apt-get remove libre-office-math -y
-sudo apt-get remove libre-office-calc -y
-sudo apt-get remove unity-scope-gdrive -y
-sudo apt-get remove libre-office-impress -y
-sudo apt-get remove brasero -y
-sudo apt-get remove cheese -y
-sudo apt-get remove rhythmbox -y
-sudo apt-get remove unity-webapps-common -y
+echo 'update package sources'
+echo ''
+sudo apt-get update -y
 
-#echo 'update package sources (assume yes)'
-#sudo apt-get update -y
+echo 'upgrade packages'
+echo ''
+sudo apt-get upgrade -y
 
-#echo 'upgrade packages (assume yes)'
-#sudo apt-get upgrade -y
+echo 'install kept-back linux packages'
+echo ''
+sudo apt-get install linux-generic -y
+sudo apt-get install linux-headers-generic -y
+sudo apt-get install linux-image-generic -y
+
+echo 'autoremove'
+echo ''
+sudo apt-get autoremove -y
+
+#echo 'remove unneeded packages'
+#sudo apt-get remove deja-dup -y
+#sudo apt-get remove gnome-bluetooth -y
+#sudo apt-get remove gnome-mahjongg -y
+#sudo apt-get remove gnome-mines -y
+#sudo apt-get remove aisleriot -y
+#sudo apt-get remove gnome-sudoku -y
+#sudo apt-get remove libre-office-draw -y
+#sudo apt-get remove webbrowser-app -y
+#sudo apt-get remove thunderbird -y
+#sudo apt-get remove libre-office-writer -y
+#sudo apt-get remove libre-office-math -y
+#sudo apt-get remove libre-office-calc -y
+#sudo apt-get remove unity-scope-gdrive -y
+#sudo apt-get remove libre-office-impress -y
+#sudo apt-get remove brasero -y
+#sudo apt-get remove cheese -y
+#sudo apt-get remove rhythmbox -y
+#sudo apt-get remove unity-webapps-common -y
+
 
 #echo 'autoremove'
 #sudo apt-get autoremove -y

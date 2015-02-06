a355c86d0c7d9638c8a943fa99148318c10d6d46
hackenfreude <hackenfreude@users.noreply.github.com>
2015-02-05 22:06:56 -0800

    reorder removal to beginning

--- machinesetup.sh
+++ machinesetup.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-echo 'update package sources (assume yes)'
-#sudo apt-get update -y
-
-echo 'upgrade packages (assume yes)'
-#sudo apt-get upgrade -y
-
-echo 'autoremove'
-#sudo apt-get autoremove -y
-
-echo 'remove what I do not want'
+echo 'remove unneeded packages'
 sudo apt-get remove deja-dup -y
 sudo apt-get remove gnome-bluetooth -y
 sudo apt-get remove gnome-mahjongg -y
@@ -29,9 +20,20 @@ sudo apt-get remove cheese -y
 sudo apt-get remove rhythmbox -y
 sudo apt-get remove unity-webapps-common -y
 
+#echo 'update package sources (assume yes)'
+#sudo apt-get update -y
+
+#echo 'upgrade packages (assume yes)'
+#sudo apt-get upgrade -y
+
+#echo 'autoremove'
+#sudo apt-get autoremove -y
+
+#echo 'remove what I do not want'
 
 
-echo 'done... for now'
+
+#echo 'done... for now'
 
 #TODO:
 #get sudo perms at beginning

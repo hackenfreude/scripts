7a22645aceb64cae535d7253d8db24af287b1643
hackenfreude <hackenfreude@users.noreply.github.com>
2015-02-07 22:53:56 -0800

    make wget recursive to get all the download targets

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #use list of download targets to get all the scripts
-wget https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/downloadtargets.txt -P scripts
+wget -i https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/downloadtargets.txt -P scripts
 
 #give execute permissions
 chmod -R +x scripts/

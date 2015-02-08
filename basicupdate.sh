46337f0bf03b4ab365e0c0ab4ef7d9e0c2352abf
hackenfreude <hackenfreude@users.noreply.github.com>
2015-02-07 22:14:14 -0800

    update bootstrapper and basic script

--- basicupdate.sh
+++ basicupdate.sh
@@ -1,55 +1,42 @@
 #!/bin/bash
 
 echo 'update package sources'
-echo ''
 sudo apt-get update -y
 
 echo 'upgrade packages'
-echo ''
 sudo apt-get upgrade -y
 
 echo 'install kept-back linux packages'
-echo ''
 sudo apt-get install linux-generic -y
 sudo apt-get install linux-headers-generic -y
 sudo apt-get install linux-image-generic -y
 
 echo 'autoremove'
-echo ''
 sudo apt-get autoremove -y
 
-#echo 'remove unneeded packages'
-#sudo apt-get remove deja-dup -y
-#sudo apt-get remove gnome-bluetooth -y
-#sudo apt-get remove gnome-mahjongg -y
-#sudo apt-get remove gnome-mines -y
-#sudo apt-get remove aisleriot -y
-#sudo apt-get remove gnome-sudoku -y
-#sudo apt-get remove libre-office-draw -y
-#sudo apt-get remove webbrowser-app -y
-#sudo apt-get remove thunderbird -y
-#sudo apt-get remove libre-office-writer -y
-#sudo apt-get remove libre-office-math -y
-#sudo apt-get remove libre-office-calc -y
-#sudo apt-get remove unity-scope-gdrive -y
-#sudo apt-get remove libre-office-impress -y
-#sudo apt-get remove brasero -y
-#sudo apt-get remove cheese -y
-#sudo apt-get remove rhythmbox -y
-#sudo apt-get remove unity-webapps-common -y
+echo 'remove unneeded packages'
+sudo apt-get remove deja-dup -y
+sudo apt-get remove gnome-bluetooth -y
+sudo apt-get remove gnome-mahjongg -y
+sudo apt-get remove gnome-mines -y
+sudo apt-get remove aisleriot -y
+sudo apt-get remove gnome-sudoku -y
+sudo apt-get remove libre-office-draw -y
+sudo apt-get remove webbrowser-app -y
+sudo apt-get remove thunderbird -y
+sudo apt-get remove libre-office-writer -y
+sudo apt-get remove libre-office-math -y
+sudo apt-get remove libre-office-calc -y
+sudo apt-get remove unity-scope-gdrive -y
+sudo apt-get remove libre-office-impress -y
+sudo apt-get remove brasero -y
+sudo apt-get remove cheese -y
+sudo apt-get remove rhythmbox -y
+sudo apt-get remove unity-webapps-common -y
 
-
-#echo 'autoremove'
-#sudo apt-get autoremove -y
-
-#echo 'remove what I do not want'
-
-
-
-#echo 'done... for now'
+echo 'autoremove'
+sudo apt-get autoremove -y
 
 #TODO:
 #get sudo perms at beginning
-#get linux generic
-#remove programs I don't want
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 !#/bin/bash
 
-#use list of download targets to get all the scripts and grant permissions
+#use list of download targets to get all the scripts
 wget https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/downloadtargets.txt -P scripts
+
+#give execute permissions
 chmod -R +x scripts/

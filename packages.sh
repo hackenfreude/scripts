6fe757a831984d94a29136898aab5ab6263d72f5
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-05 18:07:24 -0700

    comment out custom  package removals

--- packages.sh
+++ packages.sh
@@ -6,38 +6,41 @@ sudo apt-get update -y
 echo 'upgrade packages'
 sudo apt-get upgrade -y
 
-echo 'install kept-back linux packages'
-sudo apt-get install linux-generic -y
-sudo apt-get install linux-headers-generic -y
-sudo apt-get install linux-image-generic -y
+#echo 'install kept-back linux packages'
+#sudo apt-get install linux-generic -y
+#sudo apt-get install linux-headers-generic -y
+#sudo apt-get install linux-image-generic -y
 
-echo 'add other kept-back packages'
-sudo apt-get install liboxideqt-qmlplugin -y
-sudo apt-get install python-cupshelpers -y
-sudo apt-get install system-config-printer-gnome -y
+#echo 'add other kept-back packages'
+#sudo apt-get install liboxideqt-qmlplugin -y
+#sudo apt-get install python-cupshelpers -y
+#sudo apt-get install system-config-printer-gnome -y
 
-echo 'remove unneeded packages'
-sudo apt-get remove deja-dup -y
-sudo apt-get remove gnome-bluetooth -y
-sudo apt-get remove gnome-mahjongg -y
-sudo apt-get remove gnome-mines -y
-sudo apt-get remove aisleriot -y
-sudo apt-get remove gnome-sudoku -y
-sudo apt-get remove libre-office-draw -y
-sudo apt-get remove webbrowser-app -y
-sudo apt-get remove thunderbird -y
-sudo apt-get remove libre-office-writer -y
-sudo apt-get remove libre-office-math -y
-sudo apt-get remove libre-office-calc -y
-sudo apt-get remove unity-scope-gdrive -y
-sudo apt-get remove libre-office-impress -y
-sudo apt-get remove brasero -y
-sudo apt-get remove cheese -y
-sudo apt-get remove rhythmbox -y
-sudo apt-get remove unity-webapps-common -y
+#echo 'remove unneeded packages'
+#sudo apt-get remove deja-dup -y
+#sudo apt-get remove gnome-bluetooth -y
+#sudo apt-get remove gnome-mahjongg -y
+#sudo apt-get remove gnome-mines -y
+#sudo apt-get remove aisleriot -y
+#sudo apt-get remove gnome-sudoku -y
+#sudo apt-get remove libre-office-draw -y
+#sudo apt-get remove webbrowser-app -y
+#sudo apt-get remove thunderbird -y
+#sudo apt-get remove libre-office-writer -y
+#sudo apt-get remove libre-office-math -y
+#sudo apt-get remove libre-office-calc -y
+#sudo apt-get remove unity-scope-gdrive -y
+#sudo apt-get remove libre-office-impress -y
+#sudo apt-get remove brasero -y
+#sudo apt-get remove cheese -y
+#sudo apt-get remove rhythmbox -y
+#sudo apt-get remove unity-webapps-common -y
 
 echo 'autoremove'
 sudo apt-get autoremove -y
+
+echo 'autoclean'
+sudo apt-get autoclean -y
 
 echo 'check for anything else kept back'
 sudo apt-get update -y

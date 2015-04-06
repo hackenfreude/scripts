8e914947cfac36f60c1264e8d699ce4cf640b279
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-05 18:44:46 -0700

    manually install kept-back; uncomment and update package removals

--- packages.sh
+++ packages.sh
@@ -6,35 +6,37 @@ sudo apt-get update -y
 echo 'upgrade packages'
 sudo apt-get upgrade -y
 
-#echo 'install kept-back linux packages'
-#sudo apt-get install linux-generic -y
-#sudo apt-get install linux-headers-generic -y
-#sudo apt-get install linux-image-generic -y
+echo 'install kept-back linux packages'
+sudo apt-get install linux-generic-lts-utopic -y
+sudo apt-get install linux-headers-generic-lts-utopic -y
+sudo apt-get install linux-image-generic-lts-utopic -y
 
-#echo 'add other kept-back packages'
-#sudo apt-get install liboxideqt-qmlplugin -y
-#sudo apt-get install python-cupshelpers -y
-#sudo apt-get install system-config-printer-gnome -y
-
-#echo 'remove unneeded packages'
-#sudo apt-get remove deja-dup -y
-#sudo apt-get remove gnome-bluetooth -y
-#sudo apt-get remove gnome-mahjongg -y
-#sudo apt-get remove gnome-mines -y
-#sudo apt-get remove aisleriot -y
-#sudo apt-get remove gnome-sudoku -y
-#sudo apt-get remove libre-office-draw -y
-#sudo apt-get remove webbrowser-app -y
-#sudo apt-get remove thunderbird -y
-#sudo apt-get remove libre-office-writer -y
-#sudo apt-get remove libre-office-math -y
-#sudo apt-get remove libre-office-calc -y
-#sudo apt-get remove unity-scope-gdrive -y
-#sudo apt-get remove libre-office-impress -y
-#sudo apt-get remove brasero -y
-#sudo apt-get remove cheese -y
-#sudo apt-get remove rhythmbox -y
-#sudo apt-get remove unity-webapps-common -y
+echo 'remove unneeded packages'
+sudo apt-get remove aisleriot -y
+sudo apt-get remove brasero -y
+sudo apt-get remove cheese -y
+sudo apt-get remove deja-dup -y
+sudo apt-get remove gnome-mahjongg -y
+sudo apt-get remove gnome-sudoku -y
+sudo apt-get remove gnomine -y
+sudo apt-get remove libreoffice-calc -y
+sudo apt-get remove libreoffice-gnome -y
+sudo apt-get remove libreoffice-impress -y
+sudo apt-get remove libreoffice-math -y
+sudo apt-get remove libreoffice-ogltrans -y
+sudo apt-get remove libreoffice-pdfimport -y
+sudo apt-get remove libreoffice-presentation-minimizer -y
+sudo apt-get remove libreoffice-style-human -y
+sudo apt-get remove libreoffice-writer -y
+sudo apt-get remove rhythmbox -y
+sudo apt-get remove rhythmbox-plugin-magnatune -y
+sudo apt-get remove shotwell -y
+sudo apt-get remove simplescan -y
+sudo apt-get remove thunderbird -y
+sudo apt-get remove thunderbird-gnome-support -y
+sudo apt-get remove totem -y
+sudo apt-get remove totem-mozilla -y
+sudo apt-get remove unity-webapps-common -y
 
 echo 'autoremove'
 sudo apt-get autoremove -y

f3b8db858346ff8e8f2f81773ec46526d0ddd72f
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-11 16:08:51 -0700

    add handling for muliple runs of this script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-#first check the version
-CURRENT=$(lsb_release -ds)
-SUPPORTED="Ubuntu 14.04.1 LTS"
+SUPPORTED_VER="Ubuntu 14.04.1 LTS"
+WANTED_VBOX_PKG="virtualbox-guest-*"
+EXPECTED_VBOX_COUNT=3
 
-if [[ $CURRENT == $SUPPORTED ]]; then
+#read the current version
+CURRENT_VER=$(lsb_release -ds)
+
+#check installed virtualbox packages
+INSTALLED_VBOX_COUNT=$(dpkg --get-selections $WANTED_VBOX_PKG | grep -c -e '\binstall')
+
+if [[ $CURRENT_VER == $SUPPORTED_VER || $INSTALLED_VBOX_COUNT == $EXPECTED_VBOX_COUNT ]]; then
+		
+	#remove scripts from prior runs if it exists
+	rm -rf scripts/
 	
 	#use list of download targets to get all the scripts
 	wget -i https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/downloadtargets.txt -P scripts
@@ -17,5 +26,5 @@ if [[ $CURRENT == $SUPPORTED ]]; then
 	./scripts/settings.sh | tee settings.log
 
 else
-	echo "You are on $CURRENT. This script only works with $SUPPORTED due to Virtualbox compatability issues."
+	echo "You are on $CURRENT_VER. This script only works with $SUPPORTED_VER due to Virtualbox compatability issues."
 fi

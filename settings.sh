800151df26c3585241425dd730b71cb0846fe7ec
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-11 14:28:34 -0700

    uncomment gsettings

--- settings.sh
+++ settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#echo "turn off online search results"
-#sudo gsettings set com.canonical.Unity.Lenses remote-content-search "none"
+echo "turn off online search results"
+gsettings set com.canonical.Unity.Lenses remote-content-search "none"
 
 echo "turn on firewall"
 sudo ufw enable

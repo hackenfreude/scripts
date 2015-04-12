11a04222f706a26ba6e4078813fbaeda7b136b05
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-11 17:25:56 -0700

    add simplier download file

---
+++ bash-machine-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $(id -u) == 0 ]; then
+	echo "You are running as root. Run this as your normal user; the script will prompt for a password only when necessary."
+	exit 1
+fi
+
+wget -nv https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/bootstrap.sh
+

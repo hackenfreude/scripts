75074e5738d62626f42188a203d7a3f8b92951eb
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-12 17:54:31 -0700

    style changes to master

--- bash-machine-setup.sh
+++ bash-machine-setup.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/env bash
 
-if [ $(id -u) == 0 ]; then
-	echo "You are running as root. Run this as your normal user; the script will prompt for a password only when necessary."
+if [[ $(id --user) == 0 ]]
+then
+	echo 'You are running as root. Run this as your normal user; the script will prompt for a password only when necessary.'
 	exit 1
 fi
 
-rm -f bootstrap.sh
+rm --force bootstrap.sh
 
-wget -nv https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/bootstrap.sh
+wget --no-verbose https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/bootstrap.sh
 
 chmod +x bootstrap.sh
 

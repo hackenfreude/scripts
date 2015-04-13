1320d68f6c5be1a707de21f0ccbfa7b3b2591f04
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-13 15:42:43 -0700

    update clojure.sh

--- clojure.sh
+++ clojure.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+sudo apt-get install default-jdk -y
+
 mkdir ~/bin --parents
 rm ~/bin/lein --force
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein --directory-prefix ~/bin --no-verbose
 chmod +x ~/bin/lein
-./~/bin/lein
+~/bin/lein
 lein_return=$?
-echo $lein_return
+exit $lein_return

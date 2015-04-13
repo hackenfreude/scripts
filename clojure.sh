fdc08bfa11749ba578198ee6dcb7c18a31e1265f
hackenfreude <hackenfreude@users.noreply.github.com>
2015-04-13 15:27:11 -0700

    fix folder structure in clojure.sh

--- clojure.sh
+++ clojure.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir bin --parents
-rm bin/lein --force
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein --directory-prefix bin --no-verbose
-chmod +x bin/lein
-.bin/lein
+mkdir ~/bin --parents
+rm ~/bin/lein --force
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein --directory-prefix ~/bin --no-verbose
+chmod +x ~/bin/lein
+./~/bin/lein
 lein_return=$?
 echo $lein_return

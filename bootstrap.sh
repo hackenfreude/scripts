cbfa33fd0f08a7927ff2a1f571f63d064b5bb705
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-03 11:03:44 -0700

    fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ then
 	echo "Unless you have taken special action, the HOST Ubuntu system is likely running the default Ubuntu repository version of Virtualbox."
 	echo "Currently, this version of Virtualbox on the HOST only supports X Server version $supported_xserver_version on GUEST systems."
 	echo "This system (the GUEST) has X Server version $current_xserver_version."
-	echo "This matters because the script will configure Virtualbox on the GUEST to allow full-screen. This fails due to X Server incompatibility."
+	echo "This matters because the script will configure Virtualbox on the GUEST to allow full-screen. This fails due to X Server incompatability."
 	echo "Ubuntu 14.04.1 comes with the supported version of X Server; Ubuntu 14.04.2 and later do not."
 	echo "Please run this script on a clean Ubuntu 14.04.1 version."
 	exit 1

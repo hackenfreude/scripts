579b21485d7fe1d29eeaac3aaae9d30d4e94c21b
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-13 21:04:02 -0700

    fix slash direction

--- keptback.sh
+++ keptback.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kept_back_packages=$(sudo apt-get upgrade -s | sed -n '\back:\,\upgraded.\p' | sed '1d;$d')
+kept_back_packages=$(sudo apt-get upgrade -s | sed -n '/back:/,/upgraded./p' | sed '1d;$d')
 
 echo $kept_back_packages
 

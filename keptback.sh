9d621182e4cb753c101416db0e778d19489706b0
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-20 22:09:23 -0700

    remove standalone kept-back package handling

--- keptback.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kept_back_packages=$(sudo apt-get upgrade -s | sed -n '/back:/,/upgraded./p' | sed '1d;$d')
-
-#echo $kept_back_packages
-
-sudo apt-get install $kept_back_packages -y

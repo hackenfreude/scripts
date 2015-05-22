cea0f7e09eb5372de8cc56d188c697178e30646c
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-21 22:29:16 -0700

    add other vimrc lines

--- vimrc.sh
+++ vimrc.sh
@@ -10,4 +10,7 @@ fi
 touch ${HOME}/.vimrc
 
 echo -e 'set nocompatible\t"ignore vi compatibility' >> ${HOME}/.vimrc
-
+echo -e 'set number\t"show line numbers' >> ${HOME}/.vimrc
+echo -e 'set showcmd\t"show last command' >> ${HOME}/.vimrc
+echo -e 'set wildmenu\t"show possible command completion' >> ${HOME}/.vimrc
+echo -e 'set hlsearch\t"highlight all matching results of search' >> ${HOME}/.vimrc

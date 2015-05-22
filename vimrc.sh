2a4ac129aebe959d8620d891de502a26253df616
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-21 22:15:36 -0700

    write first line of .vimrc

--- vimrc.sh
+++ vimrc.sh
@@ -3,6 +3,10 @@
 if [ -f ${HOME}/.vimrc ]
 then
 	rm --force ${HOME}/.vimrc.OLD
-	mv ${HOME}/.vimrc ${HOME}/.vimrc.OLD
-	echo '.vimrc alredy exists and has been backed up as .vimrc.OLD. '
+	mv ${HOME}/.vimrc ${HOME}/vimrc.OLD
+	echo '.vimrc already exists and has been backed up as vimrc.OLD. '
 fi
+
+touch ${HOME}/.vimrc
+
+echo 'set nocompatible' >> ${HOME}/.vimrc

458faf4b3e24b9a39847aa1df6633ef9ccc55633
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-31 13:08:47 -0700

    restructure packages.sh into functions

--- packages.sh
+++ packages.sh
@@ -1,26 +1,39 @@
 #!/usr/bin/env bash
 
+logfile='./bash-machine-setup/packages.log'
+rm --force $logfile
+
+preinstall
+
 function earlyexit {
 	echo "an problem occurred during ${1}"
 	exit 1
 }
 
-logfile='./bash-machine-setup/packages.log'
-rm --force $logfile
+function preinstall {
+	echo 'begin pre-installation package maintenance'
+	
+	sudo apt-get autoremove --assume-yes &>> $logfile || earlyexit 'autoremove'
+	sudo apt-get autoclean --assume-yes &>> $logfile || earlyexit 'autoclean'
+	sudo apt-get update --assume-yes &>> $logfile || earlyexit 'update'
+	sudo apt-get upgrade --assume-yes &>> $logfile || earlyexit 'upgrade'
 
-echo 'begin pre-installation package maintenance'
+	checklog || earlyexit 'pre-installation package maintenance'
+	
+	echo 'pre-installation package maintenance succeeded'
+}
 
-sudo apt-get autoremove --assume-yes &>> $logfile || earlyexit 'autoremove'
-sudo apt-get autoclean --assume-yes &>> $logfile || earlyexit 'autoclean'
-sudo apt-get update --assume-yes &>> $logfile || earlyexit 'update'
-sudo apt-get upgrade --assume-yes &>> $logfile || earlyexit 'upgrade'
-
+function checklog {
 #necessary because apt-get may emit a problem to stdout or stderr but still return 0
-preinstall_log_problem_count=$(grep --count --extended-regexp '(Err )|(W: )|(E: )' $logfile)
 
-if [[ $preinstall_log_problem_count != 0 ]]; then earlyexit 'pre-installation package maintenance'; fi
+	problem_count=$(grep --count --extended-regexp '(Err )|(W: )|(E: )' $logfile)
+	if [[ $problem_count != 0]]
+	then
+		exit 1
+	fi
+}
 
-echo 'pre-installation package maintenance succeeded'
+
 
 echo 'remove unneeded packages'
 sudo apt-get remove aisleriot brasero cheese deja-dup gnome-mahjongg gnome-sudoku gnomine libreoffice-calc libreoffice-gnome libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-style-human libreoffice-writer rhythmbox rhythmbox-plugin-magnatune shotwell simple-scan thunderbird thunderbird-gnome-support totem totem-mozilla unity-webapps-common -y

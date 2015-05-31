65d6e0b20c920b9941af69c78c94961d182179bd
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-31 16:28:00 -0700

    finish refactor of packages

--- packages.sh
+++ packages.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-function earlyexit {
-	echo "a problem occurred during ${1}"
-	exit 1
-}
+
+##### begin main functions ##### 
 
 function preinstall {
 	stepname='pre-installation package maintenance'
@@ -19,6 +17,58 @@ function preinstall {
 	echo "${stepname} succeeded"
 }
 
+function unneeded {
+	stepname='removing unneeded packages'
+	echo "begin ${stepname}"
+
+	removal_list='aisleriot brasero cheese deja-dup gnome-mahjongg gnome-sudoku gnomine libreoffice-calc libreoffice-gnome libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-style-human libreoffice-writer rhythmbox rhythmbox-plugin-magnatune shotwell simple-scan thunderbird thunderbird-gnome-support totem totem-mozilla unity-webapps-common'
+
+	sudo apt-get remove $removal_list --assume-yes &>> || earlyexit "${stepname}: remove"
+	sudo apt-get autoremove --assume-yes &>> $logfile || earlyexit "${stepname}: autoremove"
+	sudo apt-get autoclean --assume-yes &>> $logfile || earlyexit "${stepname}: autoclean"
+
+	checklog || earlyexit "${stepname}"
+	
+	echo "${stepname} succeeded"
+}
+
+function keptback {
+	stepname='installing kept-back packages'
+	echo "begin ${stepname}"
+	
+	kept_back_packages=$(sudo apt-get upgrade -s | sed -n '/back:/,/upgraded./p' | sed '1d;$d')
+	
+	sudo apt-get install $kept_back_packages --assume-yes &>> || earlyexit "${stepname}: install"
+	sudo apt-get autoremove --assume-yes &>> $logfile || earlyexit "${stepname}: autoremove"
+	sudo apt-get autoclean --assume-yes &>> $logfile || earlyexit "${stepname}: autoclean"
+
+	checklog || earlyexit "${stepname}"
+	
+	echo "${stepname} succeeded"
+}
+
+function install {
+	stepname='installing kept-back packages'
+	packages='git vim tree screen'
+	vbox_guest='virtualbox-guest-dkms'
+
+	echo "begin ${stepname}"
+
+	sudo apt-get install $packages --assume-yes &>> || earlyexit "${stepname}: install ${packages}"
+	sudo apt-get install $vbox_guest --assume-yes &>> || earlyexit "${stepname}: install ${vbox_guest}"
+	sudo apt-get autoremove --assume-yes &>> $logfile || earlyexit "${stepname}: autoremove"
+	sudo apt-get autoclean --assume-yes &>> $logfile || earlyexit "${stepname}: autoclean"
+
+	checklog || earlyexit "${stepname}"
+	
+	echo "${stepname} succeeded"
+}
+
+##### end main functions #####
+
+
+##### begin utility functions #####
+
 #necessary because apt-get may emit a problem to stdout or stderr but still return 0
 function checklog {
 	problem_count=$(grep --count --extended-regexp '(Err )|(W: )|(E: )' $logfile)
@@ -31,24 +81,21 @@ function checklog {
 	fi
 }
 
+function earlyexit {
+	echo "a problem occurred during ${1}"
+	exit 1
+}
+
+##### end utility functions #####
+
+
+
+
 logfile='./bash-machine-setup/packages.log'
 rm --force $logfile
 
 preinstall
+unneeded
+keptback
+install
 
-echo 'remove unneeded packages'
-sudo apt-get remove aisleriot brasero cheese deja-dup gnome-mahjongg gnome-sudoku gnomine libreoffice-calc libreoffice-gnome libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-style-human libreoffice-writer rhythmbox rhythmbox-plugin-magnatune shotwell simple-scan thunderbird thunderbird-gnome-support totem totem-mozilla unity-webapps-common -y
-
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
-
-echo 'install kept-back packages'
-kept_back_packages=$(sudo apt-get upgrade -s | sed -n '/back:/,/upgraded./p' | sed '1d;$d')
-sudo apt-get install $kept_back_packages -y
-
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
-
-echo 'install git, vim, tree, screen, and virtualbox guest'
-sudo apt-get install git vim tree screen -y
-sudo apt-get install virtualbox-guest-dkms -y

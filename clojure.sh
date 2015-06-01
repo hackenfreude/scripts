ba8361d7c4857278c68c49e7c5a214edf2bc5d53
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-31 18:50:51 -0700

    refactor clojure.sh

--- clojure.sh
+++ clojure.sh
@@ -1,12 +1,56 @@
 #!/usr/bin/env bash
 
-sudo apt-get update -y
-sudo apt-get install default-jdk -y
 
-mkdir ~/bin --parents
-rm ~/bin/lein --force
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein --directory-prefix ~/bin --no-verbose
-chmod +x ~/bin/lein
-~/bin/lein
-lein_return=$?
-exit $lein_return
+##### begin main functions ##### 
+
+function java {
+	stepname='getting java'
+	echo "----------begin ${stepname}----------" | tee --append $logfile
+	
+	sudo apt-get update --assume-yes &>> $logfile || earlyexit "${stepname}: update"
+	sudo apt-get install default-jdk --assume-yes &>> $logfile || earlyexit "${stepname}: install java"
+	
+	checklog || earlyexit "${stepname}"
+	
+	echo "----------${stepname} succeeded----------" | tee --append $logfile
+}
+
+function leiningen {
+	mkdir ~/bin --parents
+	rm ~/bin/lein --force
+	wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein --directory-prefix ~/bin --no-verbose
+	chmod +x ~/bin/lein
+	~/bin/lein
+	lein_return=$?
+	exit $lein_return
+}
+
+##### end main functions #####
+
+
+##### begin utility functions #####
+
+#necessary because apt-get may emit a problem to stdout or stderr but still return 0
+function checklog {
+	problem_count=$(grep --count --extended-regexp '(Err )|(W: )|(E: )' $logfile)
+	
+	if [[ $problem_count != 0 ]]
+	then
+		return 1
+	else
+		return 0
+	fi
+}
+
+function earlyexit {
+	echo "----------a problem occurred during ${1}----------" | tee --append $logfile
+	exit 1
+}
+
+##### end utility functions #####
+
+logfile='./bash-machine-setup/clojure.log'
+
+java
+lein
+

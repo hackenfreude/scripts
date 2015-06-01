f0754b6dc287c70113637e554294e6d9779f78aa
hackenfreude <hackenfreude@users.noreply.github.com>
2015-05-31 19:04:07 -0700

    refactor clojure.sh

--- clojure.sh
+++ clojure.sh
@@ -16,13 +16,16 @@ function java {
 }
 
 function leiningen {
+	stepname='installing leiningen'
+	echo "----------begin ${stepname}----------" | tee --append $logfile
+	
 	mkdir ~/bin --parents
 	rm ~/bin/lein --force
 	wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein --directory-prefix ~/bin --no-verbose
 	chmod +x ~/bin/lein
-	~/bin/lein
-	lein_return=$?
-	exit $lein_return
+	~/bin/lein &>> $logfile || earlyexit "${stepname}"
+	
+	echo "----------${stepname} succeeded----------" | tee --append $logfile
 }
 
 ##### end main functions #####

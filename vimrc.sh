29bbfe27c2440e1e24815dff415ff3221c04c10f
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-07 13:00:25 -0700

    update vimrc.sh for error handling and logging

--- vimrc.sh
+++ vimrc.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+function earlyexit {
+	echo "----------a problem occurred during ${1}----------" | tee --append $logfile
+	exit 1
+}
+
+logfile='./bash-machine-setup/vimrc.log'
+stepname='downloading vimrc'
+
+echo "----------begin ${stepname}----------" | tee --append $logfile
+
 if [ -f ${HOME}/.vimrc ]
 then
 	rm --force ${HOME}/.vimrc.OLD
@@ -7,4 +17,7 @@ then
 	echo 'the existing version of .vimrc has been backed up as vimrc.OLD. '
 fi
 
-wget --directory-prefix ${HOME} --no-verbose --output-document .vimrc https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/vimrc
+wget --no-verbose --output-document ${HOME}/.vimrc https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/vimrc &>> $logfile || earlyexit "${stepname}"
+
+echo "----------${stepname} succeeded----------" | tee --append $logfile
+

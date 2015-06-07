fc2a80e683029aa0edec637ead9e78c302ca7346
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-07 12:45:30 -0700

    fix missing . in vimrc name

--- vimrc.sh
+++ vimrc.sh
@@ -7,4 +7,4 @@ then
 	echo 'the existing version of .vimrc has been backed up as vimrc.OLD. '
 fi
 
-wget --directory-prefix ${HOME} --no-verbose --output-document vimrc https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/vimrc
+wget --directory-prefix ${HOME} --no-verbose --output-document .vimrc https://raw.githubusercontent.com/hackenfreude/bash-machine-setup/master/vimrc
